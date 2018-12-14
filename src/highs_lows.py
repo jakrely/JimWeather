23c0b460d75b6e0883fe75da9b1e5c492ebf4ca5
Jakrely <jakrely@qq.com>
2018-12-14 17:18:30 +0800

    设置图形的格式

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -16,4 +16,10 @@ with open(filename) as f:
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(highs, c='red')
 
+# 设置图形的格式
+plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
 plt.show()
