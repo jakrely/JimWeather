a4002f1f03f166844342f175a2e035ca386710c0
Jakrely <jakrely@qq.com>
2018-12-14 17:07:46 +0800

    根据数据绘制图形.

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 
 # 从文件中获取最高气温
 filename = 'sitka_weather_07-2014.csv'
@@ -11,4 +12,8 @@ with open(filename) as f:
         high = int(row[1])
         highs.append(high)
 
-    print(highs)
+# 根据数据绘制图形
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+plt.show()
