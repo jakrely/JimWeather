f5fc0ae3144002f9f3bce42bdd6d09599e4f3ca5
Jakrely <jakrely@qq.com>
2018-12-16 23:46:06 +0800

    just a try for datetime.

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -1,4 +1,6 @@
 import csv
+from datetime import datetime
+
 from matplotlib import pyplot as plt
 
 # 从文件中获取最高气温
@@ -7,18 +9,23 @@ with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    dates = []
     highs = []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%M-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # 设置图形的格式
 plt.title("Daily high temperatures, July 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
