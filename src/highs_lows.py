69169a9e393c60b5af03091d0b38658a649bdf64
Jakrely <jakrely@qq.com>
2018-12-17 13:47:35 +0800

    添加注释：获取日期。

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-# 从文件中获取最高气温
+# 从文件中获取日期、最高气温
 filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
